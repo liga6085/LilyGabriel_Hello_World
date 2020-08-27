1f9055a80d841d3a9c5c27e8efb4d470547fc1fa
liga6085 <70085102+liga6085@users.noreply.github.com>
2020-08-26 19:04:44 -0600

    Add files via upload

---
+++ LilyGabriel_Hello_World.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+print ("Hello World!")
+
+phrase = "Hello World"
+
+print (phrase)
+
+"Jumped around the room when they finally worked"
